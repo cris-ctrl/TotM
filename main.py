bbd00be174b3d720e5d57c70b8401ea6f0136401
cris-ctrl <cristianwalter21@gmail.com>
2024-05-22 21:26:19 -0300

    moved functions and fixed variables into second code

--- main.py
+++ main.py
@@ -5,36 +5,8 @@ from MIT import *
 pygame.init()
 clock = pygame.time.Clock()
 
-# init
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-bg = (50, 50, 50)
-cooldown = 70
 
-# Screen setup
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("TotM")
 
-# Load assets
-icon = pygame.image.load("assets/logos/logo_64.png")
-pygame.display.set_icon(icon)
-sprite_sheet = pygame.image.load("assets/char/mainsheet_tom.png").convert_alpha()
-
-#Image extractor from spritesheet
-def get_img(sheet, frame, width, height, scale, color):
-    image = pygame.Surface((width, height)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(color)
-    return image
-
-#rotate tool
-def rotate(frame, angle):
-    rotated = [pygame.transform.rotate(alist[frame], angle)]
-    return rotated
-
-# Load animation frames
-alist = [get_img(sprite_sheet, x, 24, 24, 2, (255, 255, 255)) for x in range(6)]
 
 # Initial variables
 run = True
